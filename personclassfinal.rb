ca1e3d1f05cc91201914dc47dba9b93d29cbf6a1
Mark Modak <mmodak@mmodak.local>
2018-08-21 14:54:51 -0600

    Original file working. .all test only passing on its own.

--- personclassfinal.rb
+++ personclassfinal.rb
@@ -2,17 +2,22 @@ require 'pry'
 class Person
 
 @@people_all = {}
+@@all = []
 
   attr_reader :first_name, :last_name, :birth_year
 
   def initialize(first_name, last_name, birth_year)
     @@people_all[first_name] = birth_year
+    @@all << self
     @first_name = first_name
     @last_name = last_name
     @birth_year = birth_year
   end
 
 # hint: def self.all will let Person.all work
+  def self.all
+    return @@all
+  end
 
   def f_name
     # puts "This person is named #{first_name}!"
@@ -22,11 +27,11 @@ class Person
   end
 
   def l_name
-    puts "Their last name is #{last_name}."
+    last_name
   end
 
   def b_year
-    puts "#{first_name} was born in #{birth_year}."
+    birth_year
   end
 
   def current_age
@@ -34,22 +39,16 @@ class Person
   end
 
   def how_old(new_year)
-    if new_year > birth_year
-    puts "#{first_name} will be #{new_year - birth_year} years old in #{new_year}."
-  elsif new_year < birth_year
-    puts "#{first_name} will be not be born for #{birth_year - new_year} more years."
-  else
-    puts "That's the year #{first_name} #{last_name} was born."
-  end
+   new_year - birth_year
   end
 
 def which_older(other)
   if birth_year < other.birth_year
-    puts "#{first_name} is older."
+    return "#{first_name} is older."
   elsif birth_year == other.birth_year
-    puts "They are the same age."
+     return "They are the same age."
   else
-    puts "#{other.first_name} is older."
+    return "#{other.first_name} is older."
   end
 end
 
@@ -62,10 +61,7 @@ end
 def self.youngest
   sorted_array = @@people_all.sort_by { |key, value| value }.reverse
   first_array = sorted_array[0]
-  puts first_array[0]
+  return first_array[0]
 end
 
 end
-
-
-
